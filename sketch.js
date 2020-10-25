4e5d4a9b07fcb05b3086e3a043c94508f8467568
kaedesasaki <71185154+kaedesasaki@users.noreply.github.com>
2020-10-25 14:50:13 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,20 @@
 //ClickedSprites
-	var pumpkins;
-	var sheeps ;
-        var kiki;
+	var kiki;
+	var lala;
+        var star;
 	
 function setup() {
   createCanvas(1000,1000); 
-  pumpkins = new Group();
-  sheeps = new Group();
   kiki = new Group();
+  lala = new Group();
+  star = new Group();
 	 
   for(var i=0 ; i<7 ; i++){
     var sprite = createSprite(random(0,width), random(0,-1000));
-    sprite.addAnimation("pumpkins", "assets/pumpkin10.png","assets/pumpkin15.png");
+    sprite.addAnimation("kiki", "assets/kiki1.png","assets/kiki4.png");
     sprite.velocity.y = 1; 
-    pumpkins.add(sprite);
-    sprite.setCollider("circle", 0,0,16); //コライダーの範囲を設定する
+    kiki.add(sprite);
+    sprite.setCollider("circle", 0,0,50); //コライダーの範囲を設定する
     sprite.onMousePressed = function() { //スプライトをクリックした時の設定をする
       this.remove(); //スプライトを消す
 	  }
@@ -22,10 +22,10 @@ function setup() {
 	
   for(var j=0 ; j<7 ; j++){
     var sprite = createSprite(random(0,width), random(0,-1000));
-    sprite.addAnimation("sheeps", "assets/sheep10.png","assets/sheep15.png");
+    sprite.addAnimation("lala", "assets/lala1.png","assets/lala4.png");
     sprite.velocity.y = 3;//速度を3に設定
-    sheeps.add(sprite);
-    sprite.setCollider("circle", 0,0,16); //コライダーの範囲を設定する
+    lala.add(sprite);
+    sprite.setCollider("circle", 0,0,50); //コライダーの範囲を設定する
     sprite.onMousePressed = function() {//スプライトをクリックした時の設定をする
       this.remove(); //スプライトを消す
     }
@@ -34,9 +34,9 @@ function setup() {
 
 for(var k=0 ; k<7 ; k++){
     var sprite = createSprite(random(0,width), random(0,-1000));
-    sprite.addAnimation("kiki", "assets/kiki1.png","assets/kiki4.png");
+    sprite.addAnimation("star", "assets/star1.png","assets/star4.png");
     sprite.velocity.y = 5; //速度を5に設定
-    kiki.add(sprite);
+    star.add(sprite);
     sprite.setCollider("circle", 0,0,50);  //コライダーの範囲を設定する
     sprite.onMousePressed = function() { //スプライトをクリックした時の設定をする
       this.remove(); //スプライトを消す
@@ -46,19 +46,19 @@ for(var k=0 ; k<7 ; k++){
 	
 function draw() {
   background(255);
-  for(var i = 0; i<pumpkins.length; i++) {
-    var p = pumpkins[i];
+  for(var i = 0; i<kiki.length; i++) {
+    var p = kiki[i];
     if(p.position.y > height)
       p.position.y = 0;
   }
-  for(var j = 0; j<sheeps.length; j++) {
-    var s = sheeps[j];
+  for(var j = 0; j<lala.length; j++) {
+    var s = lala[j];
     if(s.position.y > height)
       s.position.y = 0;
   }
 
-for(var k = 0; k<kiki.length; k++) {
-    var g = kiki[k];
+for(var k = 0; k<star.length; k++) {
+    var g = star[k];
     if(g.position.y > height)
     g.position.y = 0;
   }
